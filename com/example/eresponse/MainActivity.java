278f00f61022e6eb53ce7eef6d763cb3a4284e74
malihawalji <mwalji249@gmail.com>
2023-03-11 17:18:33 -0800

    little changes

--- com/example/eresponse/MainActivity.java
+++ com/example/eresponse/MainActivity.java
@@ -6,9 +6,26 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1, btn2, btn3, btn4, btn5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.start);
+        b1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(MainActivity.this,symptoms.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
