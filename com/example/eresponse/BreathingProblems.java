24db857b7322f4377069e940de6bce217cd74fec
JinChun5239 <905050505r@gmail.com>
2023-03-11 18:25:41 -0800

    started on breathing prbolems

---
+++ com/example/eresponse/BreathingProblems.java
@@ -0,0 +1,15 @@
+package com.example.eresponse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SymptomsPage extends AppCompatActivity {
+
+    Button btn1, btn2, btn3, btn4, btn5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.symptoms);
+    }
+}
